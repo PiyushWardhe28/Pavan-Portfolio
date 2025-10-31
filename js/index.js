f99f42a02c909eea3ea5d21c01a58126cb7c2fe3
Piyush Wardhe <130405012+PiyushWardhe28@users.noreply.github.com>
2025-10-31 23:18:16 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // You will need to create `App.js` in the same folder or adjust the path.
+
+ReactDOM.render(<App />, document.getElementById('root'));
